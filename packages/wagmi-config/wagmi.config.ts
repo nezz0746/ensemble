d8662a6dff5ad993b2c44f72259a9ffc655c643b
nezz0746 <nezz0746@gmail.com>
2023-11-17 17:10:51 +0100

    config: update wagmi foundry configs

--- packages/wagmi-config/wagmi.config.ts
+++ packages/wagmi-config/wagmi.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "@wagmi/cli";
-import { foundry } from "@wagmi/cli/plugins";
-import { react } from "@wagmi/cli/plugins";
+import { foundry, react, erc } from "@wagmi/cli/plugins";
 import fs from "fs/promises";
 import { Address } from "wagmi";
 
@@ -42,12 +41,16 @@ export default defineConfig(async () => {
           "Map.sol/*.json",
           "ERC6551Registry.sol/*.json",
           "AccountProxy.sol/*.json",
+          "LocationTile.sol/*.json",
         ],
         deployments,
       }),
       react({
         usePrepareContractFunctionWrite: true,
         usePrepareContractWrite: true,
+      }),
+      erc({
+        721: true,
       }),
     ],
   };
